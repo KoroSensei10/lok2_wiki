51e8756ea24c2fc0d8998bd568f9ae6f2ef1823c
Mathis Jung <jungmathis02@gmail.com>
2024-05-14 23:53:14 +0200

    vercel json and fixes

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
-import { getJsFile } from './getJsFile';
+import { getJsFile } from './getJsFile.js';
 async function init(selector = '.contents') {
     await new Promise((resolve) => setTimeout(resolve, 300));
     const contents = document.querySelector(selector);
