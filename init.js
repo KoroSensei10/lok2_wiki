b49a50c119aa9a9deb63e2b5176efde52a267bbb
Mathis Jung <jungmathis02@gmail.com>
2024-05-15 00:07:04 +0200

    fixes

--- init.js
+++ init.js
@@ -1,11 +1,9 @@
-import { getJsFile } from './getJsFile.js';
 async function init(selector = '.contents') {
     await new Promise((resolve) => setTimeout(resolve, 300));
     const contents = document.querySelector(selector);
     if (!contents) {
         return init();
     }
-    getJsFile();
     return contents;
 }
 
