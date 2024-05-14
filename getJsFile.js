ffc411a9322673b268b9db522d82b5d0e4c73dd7
Mathis Jung <jungmathis02@gmail.com>
2024-05-15 00:01:54 +0200

    getJsFileInit

--- getJsFile.js
+++ getJsFile.js
@@ -1,8 +1,9 @@
 // importe dynamiquement le fichier js qui correspond au nom de la page et l'execute
 
-export const getJsFile = () => {
+const getJsFile = () => {
     const path = window.location.pathname.split('/').pop();
     const script = document.createElement('script');
     script.src = `https://lok2-wiki-js.vercel.app/pages/${path}.js`;
     document.head.appendChild(script);
 };
+getJsFile();
